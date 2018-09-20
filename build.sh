6b22831806a553b1981818666a2916aa4bd3d390
Bucket <bucket@zerodg.net>
2018-09-19 20:48:46 -0500

    honestly why

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
     
-    if [ ! -f "certbot" ]; then
-    echo "You do not have certbot installed! Install it from somewhere...google it!"
-
-    else
-
-    echo "Automatically updating the cert..."
+echo "Automatically updating the cert..."
     sleep 2
 
     fqdn () {
@@ -19,4 +14,3 @@
 
     echo "Renewal complete"
     sleep 2
-fi
