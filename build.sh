fed8347c64affadc027454f17809b59bb1fdfbba
Bucket <bucket@zerodg.net>
2018-09-19 20:38:55 -0500

    Added Bash file

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+    
+    if[! -f "certbot" ]
+    echo "You do not have certbot installed! Install it from somewhere...google it!"
+
+    else
+
+    echo "Automatically updating the cert..."
+    sleep 2
+
+    fqdn () {
+    echo $(hostname --fqdn)
+    }
+
+    echo "Running commands..."
+    systemctl apache2 stop
+    systemctl nginx stop
+    certbot renew
+
+    echo "Renewal complete"
+    sleep 2
+fi
